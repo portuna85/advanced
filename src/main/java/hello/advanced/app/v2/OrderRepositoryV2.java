941711ef50a223d2d3efab574ce8fbe6670cf15d
portuna85 <portuna85@gmail.com>
2022-10-09 23:54:34 +0900

    Spring LogTrace

--- src/main/java/hello/advanced/app/v2/OrderRepositoryV2.java
+++ src/main/java/hello/advanced/app/v2/OrderRepositoryV2.java
@@ -1,29 +1,25 @@
-package hello.advanced.app.v1;
+package hello.advanced.app.v2;
 
+import hello.advanced.trace.TraceId;
 import hello.advanced.trace.TraceStatus;
-import hello.advanced.trace.hellotrace.HelloTraceV1;
+import hello.advanced.trace.hellotrace.HelloTraceV2;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
-public class OrderRepositoryV1 {
+public class OrderRepositoryV2 {
+    private final HelloTraceV2 trace;
 
-    private final HelloTraceV1 trace;
-
-    public void save(String itemId) {
-
+    public void save(TraceId traceId, String itemId) {
         TraceStatus status = null;
-
         try {
-            status = trace.begin("OrderRepositoryV1.save()");
-
+            status = trace.beginSync(traceId, "OrderRepository.save()");
             // 저장 로직
             if (itemId.equals("ex")) {
-                throw new IllegalStateException("예외 발생");
+                throw new IllegalStateException("예외 발생!");
             }
             sleep(1000);
-
             trace.end(status);
         } catch (Exception e) {
             trace.exception(status, e);
