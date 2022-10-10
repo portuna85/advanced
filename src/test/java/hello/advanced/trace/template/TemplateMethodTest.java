4cc7cdcbcdea2411ee7c36183ad200a51d186112
portuna85 <portuna85@gmail.com>
2022-10-10 21:11:01 +0900

    Template Method Pattern

--- src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -51,4 +51,27 @@ class TemplateMethodTest {
         AbstractTemplate template2 = new SubClassLogic2();
         template2.execute();
     }
+
+    @Test
+    void templateMethodV2() {
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비지니스 로직1 실행");
+            }
+        };
+
+        log.info("클래스 이름 = {}", template1.getClass());
+        template1.execute();
+
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비지니스 로직1 실행");
+            }
+        };
+
+        log.info("클래스 이름 = {}", template2.getClass());
+        template2.execute();
+    }
 }
