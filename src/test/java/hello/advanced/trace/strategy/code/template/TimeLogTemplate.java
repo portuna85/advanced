8f75a870f0d30eec2b2110f1fef6fdb05e7b3391
portuna85 <portuna85@gmail.com>
2022-10-11 13:57:44 +0900

    Tamplate Callback

--- src/test/java/hello/advanced/trace/strategy/code/template/TimeLogTemplate.java
+++ src/test/java/hello/advanced/trace/strategy/code/template/TimeLogTemplate.java
@@ -1,24 +1,15 @@
-package hello.advanced.trace.strategy.code.strategy;
+package hello.advanced.trace.strategy.code.template;
 
 import lombok.extern.slf4j.Slf4j;
 
-/**
- * 필드에 전략을 보관하는 방식
- */
 @Slf4j
-public class ContextV1 {
+public class TimeLogTemplate {
 
-    private Strategy strategy;
-
-    public ContextV1(Strategy strategy) {
-        this.strategy = strategy;
-    }
-
-    public void execute() {
+    public void execute(Callback callback) {
         long startTime = System.currentTimeMillis();
 
         // 비즈니스 로직 실행
-        strategy.call();
+        callback.call();
 
         // 비즈니스 로직 종료
         long endTime = System.currentTimeMillis();
